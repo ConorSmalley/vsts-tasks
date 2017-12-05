microsoft/azure-pipelines-tasks

Telemetry for NuGet only

Tasks/NuGetCommand/Common/VstsNuGetPushToolRunner.ts
@@ -2,6 +2,7 @@ import {IExecOptions, IExecSyncResult, ToolRunner} from "vsts-task-lib/toolrunne
 import * as auth from "nuget-task-common/Authentication";
 import * as tl from "vsts-task-lib/task";
 import * as path from "path";
+import * as telemetry from 'utility-common/telemetry';
 
 export interface VstsNuGetPushSettings {
     continueOnConflict: boolean;
@@ -37,8 +38,11 @@ export class VstsNuGetPushToolRunner extends ToolRunner {
 
     public execSync(options?: IExecOptions): IExecSyncResult {
         options = initializeExecutionOptions(options, this.settings);
-
-        return super.execSync(options);
+        let execResult = super.execSync(options);
+        if (execResult.code !== 0) {
+            telemetry.logExecResults(execResult.code, execResult.stderr);
+        }
+        return execResult;
     }
 
     public exec(options?: IExecOptions): Q.Promise<number> {

Tasks/NuGetCommand/nugetcustom.ts
@@ -7,6 +7,8 @@ import * as auth from "nuget-task-common/Authentication";
 import locationHelpers = require("nuget-task-common/LocationHelpers");
 import nuGetGetter = require("nuget-task-common/NuGetToolGetter");
 import peParser = require('nuget-task-common/pe-parser/index');
+import {IExecSyncResult} from "vsts-task-lib/toolrunner";
+import * as telemetry from 'utility-common/telemetry';
 
 class NuGetExecutionOptions {
     constructor(
@@ -65,7 +67,7 @@ export async function run(nuGetPath: string): Promise<void> {
             args,
             authInfo);
 
-        await runNuGetAsync(executionOptions);
+        runNuGet(executionOptions);
     } catch (err) {
         tl.error(err);
 
@@ -77,10 +79,14 @@ export async function run(nuGetPath: string): Promise<void> {
     }
 }
 
-function runNuGetAsync(executionOptions: NuGetExecutionOptions): Q.Promise<number> {
+function runNuGet(executionOptions: NuGetExecutionOptions): IExecSyncResult {
     let nugetTool = ngToolRunner.createNuGetToolRunner(executionOptions.nuGetPath, executionOptions.environment, executionOptions.authInfo);
     nugetTool.line(executionOptions.args);
     nugetTool.arg("-NonInteractive");
 
-    return nugetTool.exec();
+    let execResult = nugetTool.execSync();
+    if (execResult.code !== 0) {
+        telemetry.logExecResults(execResult.code, execResult.stderr);
+    }
+    return execResult;
 }

Tasks/NuGetCommand/nugetpack.ts
@@ -5,6 +5,8 @@ import * as path from "path";
 import * as ngToolRunner from "nuget-task-common/NuGetToolRunner2";
 import * as packUtils from "nuget-task-common/PackUtilities";
 import INuGetCommandOptions from "./Common/INuGetCommandOptions";
+import {IExecSyncResult} from "vsts-task-lib/toolrunner";
+import * as telemetry from 'utility-common/telemetry';
 
 class PackOptions implements INuGetCommandOptions {
     constructor(
@@ -37,7 +39,7 @@ export async function run(nuGetPath: string): Promise<void> {
     let createSymbolsPackage = tl.getBoolInput("includeSymbols");
     let outputDir = undefined;
 
-    try 
+    try
     {
         // If outputDir is not provided then the root working directory is set by default.
         // By requiring it, it will throw an error if it is not provided and we can set it to undefined.
@@ -98,7 +100,7 @@ export async function run(nuGetPath: string): Promise<void> {
                 {
                     tl.warning(tl.loc("Warning_MoreThanOneVersionInBuildNumber"))
                 }
-                
+
                 version = versionMatches[0];
                 break;
         }
@@ -155,7 +157,7 @@ export async function run(nuGetPath: string): Promise<void> {
             environmentSettings);
 
         for (const file of filesList) {
-            await packAsync(file, packOptions);
+            pack(file, packOptions);
         }
     } catch (err) {
         tl.error(err);
@@ -163,7 +165,7 @@ export async function run(nuGetPath: string): Promise<void> {
     }
 }
 
-function packAsync(file: string, options: PackOptions): Q.Promise<number> {
+function pack(file: string, options: PackOptions): IExecSyncResult {
     console.log(tl.loc("Info_AttemptingToPackFile") + file);
 
     let nugetTool = ngToolRunner.createNuGetToolRunner(options.nuGetPath, options.environment, undefined);
@@ -198,5 +200,9 @@ function packAsync(file: string, options: PackOptions): Q.Promise<number> {
         nugetTool.arg(options.verbosity);
     }
 
-    return nugetTool.exec();
+    let execResult = nugetTool.execSync();
+    if (execResult.code !== 0) {
+        telemetry.logExecResults(execResult.code, execResult.stderr);
+    }
+    return execResult;
 }

Tasks/NuGetCommand/nugetpublisher.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as Q  from "q";
 import * as tl from "vsts-task-lib/task";
+import {IExecSyncResult} from "vsts-task-lib/toolrunner";
 
 import INuGetCommandOptions from "./Common/INuGetCommandOptions";
 import locationHelpers = require("nuget-task-common/LocationHelpers");
@@ -18,6 +19,7 @@ import * as auth from "nuget-task-common/Authentication";
 import { IPackageSource } from "nuget-task-common/Authentication";
 import peParser = require('nuget-task-common/pe-parser/index');
 import * as commandHelper from "nuget-task-common/CommandHelper";
+import * as telemetry from 'utility-common/telemetry';
 
 class PublishOptions implements INuGetCommandOptions {
     constructor(
@@ -206,8 +208,7 @@ export async function run(nuGetPath: string): Promise<void> {
                     environmentSettings);
 
                 for (const packageFile of filesList) {
-
-                    await publishPackageNuGetAsync(packageFile, publishOptions, authInfo);
+                    publishPackageNuGet(packageFile, publishOptions, authInfo);
                 }
             }
 
@@ -228,7 +229,7 @@ export async function run(nuGetPath: string): Promise<void> {
     }
 }
 
-function publishPackageNuGetAsync(packageFile: string, options: PublishOptions, authInfo: auth.NuGetExtendedAuthInfo): Q.Promise<number> {
+function publishPackageNuGet(packageFile: string, options: PublishOptions, authInfo: auth.NuGetExtendedAuthInfo): IExecSyncResult {
     let nugetTool = ngToolRunner.createNuGetToolRunner(options.nuGetPath, options.environment, authInfo);
     nugetTool.arg("push");
 
@@ -250,7 +251,11 @@ function publishPackageNuGetAsync(packageFile: string, options: PublishOptions, 
         nugetTool.arg(options.verbosity);
     }
 
-    return nugetTool.exec();
+    let execResult = nugetTool.execSync();
+    if (execResult.code !== 0) {
+        telemetry.logExecResults(execResult.code, execResult.stderr);
+    }
+    return execResult;
 }
 
 async function publishPackageVstsNuGetPushAsync(packageFile: string, options: IVstsNuGetPushOptions) {

Tasks/NuGetCommand/nugetrestore.ts
@@ -1,7 +1,7 @@
 import * as tl from "vsts-task-lib/task";
 import * as path from "path";
 import * as Q  from "q";
-import {IExecOptions} from "vsts-task-lib/toolrunner";
+import {IExecOptions, IExecSyncResult} from "vsts-task-lib/toolrunner";
 
 import * as auth from "nuget-task-common/Authentication";
 import { IPackageSource } from "nuget-task-common/Authentication";
@@ -16,6 +16,7 @@ import * as vsom from 'vso-node-api/VsoClient';
 import peParser = require('nuget-task-common/pe-parser/index');
 import {VersionInfo} from "nuget-task-common/pe-parser/VersionResource";
 import * as commandHelper from "nuget-task-common/CommandHelper";
+import * as telemetry from 'utility-common/telemetry';
 
 class RestoreOptions implements INuGetCommandOptions {
     constructor(
@@ -60,7 +61,7 @@ export async function run(nuGetPath: string): Promise<void> {
         if (!tl.filePathSupplied("packagesDirectory")) {
             packagesDirectory = null;
         }
-        
+
         const nuGetVersion: VersionInfo = await peParser.getFileVersionInfoAsync(nuGetPath);
 
         // Discovering NuGet quirks based on the version
@@ -71,7 +72,7 @@ export async function run(nuGetPath: string): Promise<void> {
         // is unconditionally displayed
         const useCredProvider = ngToolRunner.isCredentialProviderEnabled(quirks) && credProviderPath;
         const useCredConfig = ngToolRunner.isCredentialConfigEnabled(quirks) && !useCredProvider;
-        
+
         // Setting up auth-related variables
         tl.debug('Setting up auth');
         let serviceUri = tl.getEndpointUrl("SYSTEMVSSCONNECTION", false);
@@ -104,7 +105,7 @@ export async function run(nuGetPath: string): Promise<void> {
                 nuGetConfigPath = undefined;
             }
         }
-        
+
         // If there was no nuGetConfigPath, NuGetConfigHelper will create a temp one
         let nuGetConfigHelper = new NuGetConfigHelper2(
                     nuGetPath,
@@ -112,9 +113,9 @@ export async function run(nuGetPath: string): Promise<void> {
                     authInfo,
                     environmentSettings,
                     null);
-        
+
         let credCleanup = () => { return; };
-        
+
         // Now that the NuGetConfigHelper was initialized with all the known information we can proceed
         // and check if the user picked the 'select' option to fill out the config file if needed
         if (selectOrConfig === "select" ) {
@@ -174,7 +175,7 @@ export async function run(nuGetPath: string): Promise<void> {
                 authInfo);
 
             for (const solutionFile of filesList) {
-                await restorePackagesAsync(solutionFile, restoreOptions);
+                restorePackages(solutionFile, restoreOptions);
             }
         } finally {
             credCleanup();
@@ -192,7 +193,7 @@ export async function run(nuGetPath: string): Promise<void> {
     }
 }
 
-function restorePackagesAsync(solutionFile: string, options: RestoreOptions): Q.Promise<number> {
+function restorePackages(solutionFile: string, options: RestoreOptions): IExecSyncResult {
     let nugetTool = ngToolRunner.createNuGetToolRunner(options.nuGetPath, options.environment, options.authInfo);
 
     nugetTool.arg("restore");
@@ -218,6 +219,10 @@ function restorePackagesAsync(solutionFile: string, options: RestoreOptions): Q.
         nugetTool.arg("-ConfigFile");
         nugetTool.arg(options.configFile);
     }
-    
-    return nugetTool.exec({ cwd: path.dirname(solutionFile) } as IExecOptions);
+
+    let execResult = nugetTool.execSync({ cwd: path.dirname(solutionFile) } as IExecOptions);
+    if (execResult.code !== 0) {
+        telemetry.logExecResults(execResult.code, execResult.stderr);
+    }
+    return execResult;
 }

ConorSmalley/vsts-tasks

Tasks/NuGetCommand/Common/VstsNuGetPushToolRunner.ts
@@ -2,6 +2,7 @@ import {IExecOptions, IExecSyncResult, ToolRunner} from "vsts-task-lib/toolrunne
 import * as auth from "nuget-task-common/Authentication";
 import * as tl from "vsts-task-lib/task";
 import * as path from "path";
+import * as telemetry from 'utility-common/telemetry';
 
 export interface VstsNuGetPushSettings {
     continueOnConflict: boolean;
@@ -37,8 +38,11 @@ export class VstsNuGetPushToolRunner extends ToolRunner {
 
     public execSync(options?: IExecOptions): IExecSyncResult {
         options = initializeExecutionOptions(options, this.settings);
-
-        return super.execSync(options);
+        let execResult = super.execSync(options);
+        if (execResult.code !== 0) {
+            telemetry.logStderr(execResult.code, execResult.stderr);
+        }
+        return execResult;
     }
 
     public exec(options?: IExecOptions): Q.Promise<number> {

Tasks/NuGetCommand/nugetcustom.ts
@@ -7,6 +7,8 @@ import * as auth from "nuget-task-common/Authentication";
 import locationHelpers = require("nuget-task-common/LocationHelpers");
 import nuGetGetter = require("nuget-task-common/NuGetToolGetter");
 import peParser = require('nuget-task-common/pe-parser/index');
+import {IExecSyncResult} from "vsts-task-lib/toolrunner";
+import * as telemetry from 'utility-common/telemetry';
 
 class NuGetExecutionOptions {
     constructor(
@@ -65,7 +67,7 @@ export async function run(nuGetPath: string): Promise<void> {
             args,
             authInfo);
 
-        await runNuGetAsync(executionOptions);
+        runNuGet(executionOptions);
     } catch (err) {
         tl.error(err);
 
@@ -77,10 +79,14 @@ export async function run(nuGetPath: string): Promise<void> {
     }
 }
 
-function runNuGetAsync(executionOptions: NuGetExecutionOptions): Q.Promise<number> {
+function runNuGet(executionOptions: NuGetExecutionOptions): IExecSyncResult {
     let nugetTool = ngToolRunner.createNuGetToolRunner(executionOptions.nuGetPath, executionOptions.environment, executionOptions.authInfo);
     nugetTool.line(executionOptions.args);
     nugetTool.arg("-NonInteractive");
 
-    return nugetTool.exec();
+    let execResult = nugetTool.execSync();
+    if (execResult.code !== 0) {
+        telemetry.logStderr(execResult.code, execResult.stderr);
+    }
+    return execResult;
 }

Tasks/NuGetCommand/nugetpack.ts
@@ -5,6 +5,8 @@ import * as path from "path";
 import * as ngToolRunner from "nuget-task-common/NuGetToolRunner2";
 import * as packUtils from "nuget-task-common/PackUtilities";
 import INuGetCommandOptions from "./Common/INuGetCommandOptions";
+import {IExecSyncResult} from "vsts-task-lib/toolrunner";
+import * as telemetry from 'utility-common/telemetry';
 
 class PackOptions implements INuGetCommandOptions {
     constructor(
@@ -37,7 +39,7 @@ export async function run(nuGetPath: string): Promise<void> {
     let createSymbolsPackage = tl.getBoolInput("includeSymbols");
     let outputDir = undefined;
 
-    try 
+    try
     {
         // If outputDir is not provided then the root working directory is set by default.
         // By requiring it, it will throw an error if it is not provided and we can set it to undefined.
@@ -98,7 +100,7 @@ export async function run(nuGetPath: string): Promise<void> {
                 {
                     tl.warning(tl.loc("Warning_MoreThanOneVersionInBuildNumber"))
                 }
-                
+
                 version = versionMatches[0];
                 break;
         }
@@ -155,7 +157,7 @@ export async function run(nuGetPath: string): Promise<void> {
             environmentSettings);
 
         for (const file of filesList) {
-            await packAsync(file, packOptions);
+            pack(file, packOptions);
         }
     } catch (err) {
         tl.error(err);
@@ -163,7 +165,7 @@ export async function run(nuGetPath: string): Promise<void> {
     }
 }
 
-function packAsync(file: string, options: PackOptions): Q.Promise<number> {
+function pack(file: string, options: PackOptions): IExecSyncResult {
     console.log(tl.loc("Info_AttemptingToPackFile") + file);
 
     let nugetTool = ngToolRunner.createNuGetToolRunner(options.nuGetPath, options.environment, undefined);
@@ -198,5 +200,9 @@ function packAsync(file: string, options: PackOptions): Q.Promise<number> {
         nugetTool.arg(options.verbosity);
     }
 
-    return nugetTool.exec();
+    let execResult = nugetTool.execSync();
+    if (execResult.code !== 0) {
+        telemetry.logStderr(execResult.code, execResult.stderr);
+    }
+    return execResult;
 }

Tasks/NuGetCommand/nugetpublisher.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as Q  from "q";
 import * as tl from "vsts-task-lib/task";
+import {IExecSyncResult} from "vsts-task-lib/toolrunner";
 
 import INuGetCommandOptions from "./Common/INuGetCommandOptions";
 import locationHelpers = require("nuget-task-common/LocationHelpers");
@@ -18,6 +19,7 @@ import * as auth from "nuget-task-common/Authentication";
 import { IPackageSource } from "nuget-task-common/Authentication";
 import peParser = require('nuget-task-common/pe-parser/index');
 import * as commandHelper from "nuget-task-common/CommandHelper";
+import * as telemetry from 'utility-common/telemetry';
 
 class PublishOptions implements INuGetCommandOptions {
     constructor(
@@ -206,8 +208,7 @@ export async function run(nuGetPath: string): Promise<void> {
                     environmentSettings);
 
                 for (const packageFile of filesList) {
-
-                    await publishPackageNuGetAsync(packageFile, publishOptions, authInfo);
+                    publishPackageNuGet(packageFile, publishOptions, authInfo);
                 }
             }
 
@@ -228,7 +229,7 @@ export async function run(nuGetPath: string): Promise<void> {
     }
 }
 
-function publishPackageNuGetAsync(packageFile: string, options: PublishOptions, authInfo: auth.NuGetExtendedAuthInfo): Q.Promise<number> {
+function publishPackageNuGet(packageFile: string, options: PublishOptions, authInfo: auth.NuGetExtendedAuthInfo): IExecSyncResult {
     let nugetTool = ngToolRunner.createNuGetToolRunner(options.nuGetPath, options.environment, authInfo);
     nugetTool.arg("push");
 
@@ -250,7 +251,11 @@ function publishPackageNuGetAsync(packageFile: string, options: PublishOptions, 
         nugetTool.arg(options.verbosity);
     }
 
-    return nugetTool.exec();
+    let execResult = nugetTool.execSync();
+    if (execResult.code !== 0) {
+        telemetry.logStderr(execResult.code, execResult.stderr);
+    }
+    return execResult;
 }
 
 async function publishPackageVstsNuGetPushAsync(packageFile: string, options: IVstsNuGetPushOptions) {

Tasks/NuGetCommand/nugetrestore.ts
@@ -1,7 +1,7 @@
 import * as tl from "vsts-task-lib/task";
 import * as path from "path";
 import * as Q  from "q";
-import {IExecOptions} from "vsts-task-lib/toolrunner";
+import {IExecOptions, IExecSyncResult} from "vsts-task-lib/toolrunner";
 
 import * as auth from "nuget-task-common/Authentication";
 import { IPackageSource } from "nuget-task-common/Authentication";
@@ -16,6 +16,7 @@ import * as vsom from 'vso-node-api/VsoClient';
 import peParser = require('nuget-task-common/pe-parser/index');
 import {VersionInfo} from "nuget-task-common/pe-parser/VersionResource";
 import * as commandHelper from "nuget-task-common/CommandHelper";
+import * as telemetry from 'utility-common/telemetry';
 
 class RestoreOptions implements INuGetCommandOptions {
     constructor(
@@ -60,7 +61,7 @@ export async function run(nuGetPath: string): Promise<void> {
         if (!tl.filePathSupplied("packagesDirectory")) {
             packagesDirectory = null;
         }
-        
+
         const nuGetVersion: VersionInfo = await peParser.getFileVersionInfoAsync(nuGetPath);
 
         // Discovering NuGet quirks based on the version
@@ -71,7 +72,7 @@ export async function run(nuGetPath: string): Promise<void> {
         // is unconditionally displayed
         const useCredProvider = ngToolRunner.isCredentialProviderEnabled(quirks) && credProviderPath;
         const useCredConfig = ngToolRunner.isCredentialConfigEnabled(quirks) && !useCredProvider;
-        
+
         // Setting up auth-related variables
         tl.debug('Setting up auth');
         let serviceUri = tl.getEndpointUrl("SYSTEMVSSCONNECTION", false);
@@ -104,7 +105,7 @@ export async function run(nuGetPath: string): Promise<void> {
                 nuGetConfigPath = undefined;
             }
         }
-        
+
         // If there was no nuGetConfigPath, NuGetConfigHelper will create a temp one
         let nuGetConfigHelper = new NuGetConfigHelper2(
                     nuGetPath,
@@ -112,9 +113,9 @@ export async function run(nuGetPath: string): Promise<void> {
                     authInfo,
                     environmentSettings,
                     null);
-        
+
         let credCleanup = () => { return; };
-        
+
         // Now that the NuGetConfigHelper was initialized with all the known information we can proceed
         // and check if the user picked the 'select' option to fill out the config file if needed
         if (selectOrConfig === "select" ) {
@@ -174,7 +175,7 @@ export async function run(nuGetPath: string): Promise<void> {
                 authInfo);
 
             for (const solutionFile of filesList) {
-                await restorePackagesAsync(solutionFile, restoreOptions);
+                restorePackages(solutionFile, restoreOptions);
             }
         } finally {
             credCleanup();
@@ -192,7 +193,7 @@ export async function run(nuGetPath: string): Promise<void> {
     }
 }
 
-function restorePackagesAsync(solutionFile: string, options: RestoreOptions): Q.Promise<number> {
+function restorePackages(solutionFile: string, options: RestoreOptions): IExecSyncResult {
     let nugetTool = ngToolRunner.createNuGetToolRunner(options.nuGetPath, options.environment, options.authInfo);
 
     nugetTool.arg("restore");
@@ -218,6 +219,10 @@ function restorePackagesAsync(solutionFile: string, options: RestoreOptions): Q.
         nugetTool.arg("-ConfigFile");
         nugetTool.arg(options.configFile);
     }
-    
-    return nugetTool.exec({ cwd: path.dirname(solutionFile) } as IExecOptions);
+
+    let execResult = nugetTool.execSync({ cwd: path.dirname(solutionFile) } as IExecOptions);
+    if (execResult.code !== 0) {
+        telemetry.logStderr(execResult.code, execResult.stderr);
+    }
+    return execResult;
 }